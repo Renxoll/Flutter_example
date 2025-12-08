0514a295c432850e17035c83e684d280b5d49271
Renxoll <112541849+Renxoll@users.noreply.github.com>
2025-12-08 00:02:33 -0500

    feat: add theme file and implementation on main

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:example/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final MaterialTheme materialTheme = MaterialTheme(TextTheme());
+
+    return MaterialApp(
+      theme: materialTheme.light(),
+      darkTheme: materialTheme.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
